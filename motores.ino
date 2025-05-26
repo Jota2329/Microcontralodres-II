5e775e4dd9cc69efae269b7c1925de7c9aca1e9d
Jota2329 <1332023026@alumnos.unp.edu.pe>
2025-05-26 14:08:27 -0500

    Add files via upload

---
+++ motores.ino
@@ -0,0 +1,15 @@
+const int pinIN3 = 7;   // Dirección motor
+const int pinIN4 = 8;   // Dirección opuesta
+const int pinENB = 9;   // PWM (control de velocidad)
+
+void setup() {
+  pinMode(pinIN3, OUTPUT);
+  pinMode(pinIN4, OUTPUT);
+  pinMode(pinENB, OUTPUT);
+}
+
+void loop() {
+  // Sentido antihorario (izquierda)
+  digitalWrite(pinIN3, LOW);   // IN3 a 0
+  digitalWrite(pinIN4, HIGH);  // IN4 a 1
+  analogWrite(pinENB, 200);    // Velocidad moderada (0–255)
