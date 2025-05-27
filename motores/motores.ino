97a0b3a430bb7e603589ce0e434af05e21cd640a
Jota2329 <1332023026@alumnos.unp.edu.pe>
2025-05-26 23:51:55 -0500

    Actualización automática desde CMD

--- motores/motores.ino
+++ motores/motores.ino
@@ -3,13 +3,13 @@ const int pinIN4 = 8;   // Dirección opuesta
 const int pinENB = 9;   // PWM (control de velocidad)
 
 void setup() {
-  pinMode(pinIN3, OUTPUT);//josepp cabrazoo
+  pinMode(pinIN3, OUTPUT);
   pinMode(pinIN4, OUTPUT);
   pinMode(pinENB, OUTPUT);
 }
 
 void loop() {
   // Sentido horario (derecha)
-  digitalWrite(pinIN3,HIGH );   // IN3 a 0
-  digitalWrite(pinIN4,LOW );  // IN4 a 1
+  digitalWrite(pinIN3, HIGH);   // IN3 a 0
+  digitalWrite(pinIN4, LOW);  // IN4 a 1
   analogWrite(pinENB, 200);    // Velocidad moderada (0–255)
